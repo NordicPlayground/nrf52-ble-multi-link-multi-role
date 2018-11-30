240105d4aba33c4604e9d9fcd2638eca7f1edee8
Torbjørn Øvrebekk <torbjorn.ovrebekk@nordicsemi.no>
2018-11-30 14:16:26 +0100

    Central links on aggregator increased from 10 to 16. More than this appears to require SDK changes

--- ble_aggregator/pca10056/s140/config/sdk_config.h
+++ ble_aggregator/pca10056/s140/config/sdk_config.h
@@ -11818,14 +11818,14 @@
 
 // <o> NRF_SDH_BLE_CENTRAL_LINK_COUNT - Maximum number of central links. 
 #ifndef NRF_SDH_BLE_CENTRAL_LINK_COUNT
-#define NRF_SDH_BLE_CENTRAL_LINK_COUNT 10
+#define NRF_SDH_BLE_CENTRAL_LINK_COUNT 16
 #endif
 
 // <o> NRF_SDH_BLE_TOTAL_LINK_COUNT - Total link count. 
 // <i> Maximum number of total concurrent connections using the default configuration.
 
 #ifndef NRF_SDH_BLE_TOTAL_LINK_COUNT
-#define NRF_SDH_BLE_TOTAL_LINK_COUNT 11
+#define NRF_SDH_BLE_TOTAL_LINK_COUNT 17
 #endif
 
 // <o> NRF_SDH_BLE_GAP_EVENT_LENGTH - GAP event length. 
