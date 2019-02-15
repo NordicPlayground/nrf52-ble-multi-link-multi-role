a43879a0989b0eed513d5900440c50aedb78c919
Torbjørn Øvrebekk <torbjorn.ovrebekk@nordicsemi.no>
2019-02-15 15:51:41 +0100

    bat files added for flashing demo kits (aggregator, peripheral with display, and peripheral with Neopixel Matrix)

---
+++ demo_hex_files/program_peripheral_aggregator_pca10056.bat
@@ -0,0 +1,4 @@
+nrfjprog -e
+nrfjprog --program hex\s140_nrf52_6.1.1_softdevice.hex
+nrfjprog --program hex\ble_aggregator_pca10056_s140.hex
+nrfjprog -r
---
+++ demo_hex_files/program_peripheral_neopixel_matrix_pca10056.bat
@@ -0,0 +1,4 @@
+nrfjprog -e
+nrfjprog --program hex\s140_nrf52_6.1.1_softdevice.hex
+nrfjprog --program hex\ble_peripheral_lr_cl_pca10056_s140npshield.hex
+nrfjprog -r
